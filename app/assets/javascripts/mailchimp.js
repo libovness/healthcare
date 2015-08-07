1bf87056a0548865c7028f3ffcb79ffaa403d728
libovness <jonathan.libov@gmail.com>
2015-08-06 23:12:56 -0400

    email registration form is half working

--- app/assets/javascripts/mailchimp.js
+++ app/assets/javascripts/mailchimp.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
- 
+
+  $("input#email").focus(function() {
+    if($("input#email").val() == "email@example.com") {
+      $("input#email").val("");
+    }
+  });
+
   $('form#mcsubscribe')
     .bind("ajax:beforeSend", function(evt, xhr, settings){
-      var $divResponse = $('div#response');
+      var $divResponse = $('h4#caption');
  
       // Update the text of the submit button to let the user know stuff is happening.
       // But first, store the original text of the submit button, so it can be restored when the request is finished.
@@ -12,17 +18,17 @@ $(document).ready(function(){
     })
     .bind("ajax:success", function(evt, data, status, xhr){
       var $form = $(this);
-       var $divResponse = $('div#response');
+       var $divResponse = $('h4#caption');
  
       // Reset fields and any validation errors, so form can be used again, but leave hidden_field values intact.
       $form.find('input[type="email"]').val("");
       $divResponse.html(data.message);
     })
     .bind('ajax:complete', function(evt, xhr, status){
-      var $divResponse = $('div#response');
+      var $divResponse = $('h4#caption');
     })
     .bind("ajax:error", function(evt, xhr, status, error){
-       var $divResponse = $('div#response'),
+       var $divResponse = $('h4#caption'),
           errors,
           errorText,
       errorList;
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
 
   # GET /posts
   # GET /posts.json
@@ -13,7 +14,6 @@ class PostsController < ApplicationController
   end
 
   # GET /posts/new
-  def new
     @post = Post.new
   end
 
@@ -55,6 +55,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    before_action :authenticate_user!
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
