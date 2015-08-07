1934e9d5226179f326bf8e8e983cf99627dc263e
libovness <jonathan.libov@gmail.com>
2015-08-07 13:01:38 -0400

    fixed disqus

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,5 +1,5 @@
 DisqusRails.setup do |config|
-  config::SHORT_NAME = "libovness"
+  config::SHORT_NAME = "healthcare-usv-version-one"
   config::SECRET_KEY = "3oirNRo07RkdIxSGeXDOLqgJEyeSE6sxDsn2WASXOJw8Q1JVBNZqCh2kHkA7qXfR" #leave blank if not used
   config::PUBLIC_KEY = "wSQKrS8sr8GqSTboR61Y3tJVL4AKnycukns3KiDlA5ASQHxcfZcrgSfAYOXUMvjv" #leave blank if not used
   config::ACCESS_TOKEN = "aa8cfef6755747ea826dd37c2e226894" #you got it, right? ;-)
